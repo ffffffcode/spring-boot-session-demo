480d6a82b3abf1472a129506d83d51cb13c6c863
AaronFae <aaronfae@163.com>
2019-03-19 18:35:01 +0800

    Remove useless import

--- src/main/java/com/example/demo/LoginComtroller.java
+++ src/main/java/com/example/demo/LoginComtroller.java
@@ -5,7 +5,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
